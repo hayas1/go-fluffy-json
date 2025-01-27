dc6bb5c9a073fecb50a30183854ac46ef85059fc
hayas1 <h4ystack@gmail.com>
2025-01-27 12:25:22 +0000

    expired

--- as_test.go
+++ as_test.go
@@ -226,49 +226,6 @@ func TestAsValue(t *testing.T) {
 			}
 		}
 	})
-
-	testcases := []struct {
-		name   string
-		target string
-		as     func(fluffyjson.JsonValue) (fluffyjson.JsonValue, error)
-		expect fluffyjson.JsonValue
-		err    error
-	}{
-		{
-			name:   "object as object",
-			target: `{"number": ["zero", "one", "two"]}`,
-			as:     func(jv fluffyjson.JsonValue) (fluffyjson.JsonValue, error) { o, e := jv.AccessAsObject(); return &o, e },
-			expect: &fluffyjson.Object{
-				"number": &fluffyjson.Array{
-					fluffyjson.ForceString("zero"), fluffyjson.ForceString("one"), fluffyjson.ForceString("two"),
-				},
-			},
-			err: nil,
-		},
-		{
-			name:   "array as string",
-			target: `["hello", "world"]`,
-			as:     func(jv fluffyjson.JsonValue) (fluffyjson.JsonValue, error) { a, e := jv.AccessAsString(); return &a, e },
-			expect: fluffyjson.ForceString(""),
-			err:    fluffyjson.ErrAsValue{Expected: fluffyjson.STRING, Actual: fluffyjson.ARRAY},
-		},
-	}
-
-	for _, tc := range testcases {
-		t.Run(tc.name, func(t *testing.T) {
-			var value fluffyjson.RootValue
-			if err := json.Unmarshal([]byte(tc.target), &value); err != nil {
-				t.Fatal(err)
-			}
-
-			actual, err := tc.as(&value)
-			if !errors.Is(err, tc.err) {
-				t.Fatal(fmt.Errorf("%w <-> %w", tc.err, err))
-			} else if diff := cmp.Diff(tc.expect, actual); diff != "" {
-				t.Fatal(diff)
-			}
-		})
-	}
 }
 
 func TestAccessAsValue(t *testing.T) {
