5b0a9044e4bf928380abc485020cb1b518b50989
hayas1 <h4ystack@gmail.com>
2025-01-30 12:32:26 +0000

    fuzz

--- access_test.go
+++ access_test.go
@@ -7,170 +7,6 @@ import (
 	fluffyjson "github.com/hayas1/go-fluffy-json"
 )
 
-func TestAsValueLike(t *testing.T) {
-	t.Run("as object", func(t *testing.T) {
-		testcases := map[string]struct {
-			target   string
-			expected fluffyjson.Object
-			err      error
-		}{
-			"object as object": {
-				target:   `{"hello":"world"}`,
-				expected: fluffyjson.Object{"hello": HelperCastString(t, "world")},
-				err:      nil,
-			},
-			"string as object": {
-				target:   `[0,1,2,{"three":4}]`,
-				expected: nil,
-				err:      fluffyjson.ErrAsValue{Expected: fluffyjson.OBJECT, Actual: fluffyjson.ARRAY},
-			},
-		}
-
-		for name, tc := range testcases {
-			t.Run(name, func(t *testing.T) {
-				value := HelperUnmarshalValue(t, tc.target)
-				actual, err := value.AccessAsObject()
-				HelperFatalEvaluateError(t, tc.expected, actual, tc.err, err)
-			})
-		}
-	})
-
-	t.Run("as array", func(t *testing.T) {
-		testcases := map[string]struct {
-			target   string
-			expected fluffyjson.Array
-			err      error
-		}{
-			"array as array": {
-				target:   `["hello", "world"]`,
-				expected: fluffyjson.Array{HelperCastString(t, "hello"), HelperCastString(t, "world")},
-				err:      nil,
-			},
-			"string as array": {
-				target:   `"hello world"`,
-				expected: nil,
-				err:      fluffyjson.ErrAsValue{Expected: fluffyjson.ARRAY, Actual: fluffyjson.STRING},
-			},
-		}
-
-		for name, tc := range testcases {
-			t.Run(name, func(t *testing.T) {
-				value := HelperUnmarshalValue(t, tc.target)
-				actual, err := value.AccessAsArray()
-				HelperFatalEvaluateError(t, tc.expected, actual, tc.err, err)
-			})
-		}
-	})
-
-	t.Run("as string", func(t *testing.T) {
-		testcases := map[string]struct {
-			target   string
-			expected fluffyjson.String
-			err      error
-		}{
-			"string as string": {
-				target:   `"hello world"`,
-				expected: "hello world",
-				err:      nil,
-			},
-			"number as string": {
-				target:   `100`,
-				expected: "",
-				err:      fluffyjson.ErrAsValue{Expected: fluffyjson.STRING, Actual: fluffyjson.NUMBER},
-			},
-		}
-
-		for name, tc := range testcases {
-			t.Run(name, func(t *testing.T) {
-				value := HelperUnmarshalValue(t, tc.target)
-				actual, err := value.AccessAsString()
-				HelperFatalEvaluateError(t, tc.expected, actual, tc.err, err)
-			})
-		}
-	})
-
-	t.Run("as number", func(t *testing.T) {
-		testcases := map[string]struct {
-			target   string
-			expected fluffyjson.Number
-			err      error
-		}{
-			"number as number": {
-				target:   `100`,
-				expected: 100,
-				err:      nil,
-			},
-			"string as number": {
-				target:   `true`,
-				expected: 0,
-				err:      fluffyjson.ErrAsValue{Expected: fluffyjson.NUMBER, Actual: fluffyjson.BOOL},
-			},
-		}
-
-		for name, tc := range testcases {
-			t.Run(name, func(t *testing.T) {
-				value := HelperUnmarshalValue(t, tc.target)
-				actual, err := value.AccessAsNumber()
-				HelperFatalEvaluateError(t, tc.expected, actual, tc.err, err)
-			})
-		}
-	})
-
-	t.Run("as bool", func(t *testing.T) {
-		testcases := map[string]struct {
-			target   string
-			expected fluffyjson.Bool
-			err      error
-		}{
-			"bool as bool": {
-				target:   `true`,
-				expected: true,
-				err:      nil,
-			},
-			"null as bool": {
-				target:   `null`,
-				expected: false,
-				err:      fluffyjson.ErrAsValue{Expected: fluffyjson.BOOL, Actual: fluffyjson.NULL},
-			},
-		}
-
-		for name, tc := range testcases {
-			t.Run(name, func(t *testing.T) {
-				value := HelperUnmarshalValue(t, tc.target)
-				actual, err := value.AccessAsBool()
-				HelperFatalEvaluateError(t, tc.expected, actual, tc.err, err)
-			})
-		}
-	})
-
-	t.Run("as null", func(t *testing.T) {
-		testcases := map[string]struct {
-			target   string
-			expected fluffyjson.Null
-			err      error
-		}{
-			"null as null": {
-				target:   `null`,
-				expected: nil,
-				err:      nil,
-			},
-			"object as null": {
-				target:   `{"hello": "world"}`,
-				expected: nil,
-				err:      fluffyjson.ErrAsValue{Expected: fluffyjson.NULL, Actual: fluffyjson.OBJECT},
-			},
-		}
-
-		for name, tc := range testcases {
-			t.Run(name, func(t *testing.T) {
-				value := HelperUnmarshalValue(t, tc.target)
-				actual, err := value.AccessAsNull()
-				HelperFatalEvaluateError(t, tc.expected, actual, tc.err, err)
-			})
-		}
-	})
-}
-
 func TestAccessAsValue(t *testing.T) {
 	t.Run("as object", func(t *testing.T) {
 		testcases := map[string]struct {
@@ -369,5 +205,43 @@ func TestSubtreeReplace(t *testing.T) {
 		}
 		expected := `[0,{"hello":"world"},2]`
 		HelperFatalEvaluate(t, expected, string(replaced))
+	})
+}
+
+func FuzzAccessAsValueAndAsValue(f *testing.F) {
+	f.Add(`{"hello":"world"}`)
+	f.Add(`[1,2,3]`)
+	f.Add(`"string"`)
+	f.Add(`100`)
+	f.Add(`true`)
+	f.Add(`null`)
+	f.Fuzz(func(t *testing.T, target string) {
+		var value fluffyjson.RootValue
+		if err := json.Unmarshal([]byte(target), &value); err != nil {
+			return
+		}
+		asObject, errAs := value.AsObject()
+		accessAsObject, errAccessAs := value.AccessAsObject()
+		HelperFatalEvaluateError(t, asObject, accessAsObject, errAs, errAccessAs)
+
+		asArray, errAs := value.AsArray()
+		accessAsArray, errAccessAs := value.AccessAsArray()
+		HelperFatalEvaluateError(t, asArray, accessAsArray, errAs, errAccessAs)
+
+		asString, errAs := value.AsString()
+		accessAsString, errAccessAs := value.AccessAsString()
+		HelperFatalEvaluateError(t, asString, accessAsString, errAs, errAccessAs)
+
+		asNumber, errAs := value.AsNumber()
+		accessAsNumber, errAccessAs := value.AccessAsNumber()
+		HelperFatalEvaluateError(t, asNumber, accessAsNumber, errAs, errAccessAs)
+
+		asBool, errAs := value.AsBool()
+		accessAsBool, errAccessAs := value.AccessAsBool()
+		HelperFatalEvaluateError(t, asBool, accessAsBool, errAs, errAccessAs)
+
+		asNull, errAs := value.AsNull()
+		accessAsNull, errAccessAs := value.AccessAsNull()
+		HelperFatalEvaluateError(t, asNull, accessAsNull, errAs, errAccessAs)
 	})
 }
